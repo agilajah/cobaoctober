49fc67912b8cbb61876ba8f3e6936a85343784db
root <febiagil20@gmail.com>
2016-12-31 10:42:00 +0700

    creating more advanced components: adding options to it

--- plugins/cobaoctober/movies/components/Actors.php
+++ plugins/cobaoctober/movies/components/Actors.php
@@ -4,10 +4,37 @@ use Cms\Classes\ComponentBase;
 use cobaoctober\Movies\Models\Actor;
 
 class Actors extends ComponentBase {
+
   public function componentDetails() {
     return [
         'name' => 'Actor list',
         'description' => 'List of actors'
+      ];
+  }
+
+  public function defineProperties() {
+    return [
+        'results' => [
+            'title' => 'Number of actors',
+            'description' => 'How many actors do you want to display?',
+            'default' => 0,
+            'validationPattern' => '^[0-9]+$',
+            'validationMessage' => 'Only numbers allowed'
+        ],
+
+        'sortOrder' => [
+            'title' => 'Sort Actors',
+            'description' => 'Sort those actors in database',
+            'type' => 'dropdown',
+            'default' => 'name asc'
+        ]
+    ];
+  }
+
+  public function getSortOrderOptions() {
+    return [
+        'name asc' => 'Name (ascending)',
+        'name desc' => 'Name (descending)'
       ];
   }
 
@@ -16,7 +43,22 @@ class Actors extends ComponentBase {
     $this->actors = $this->loadActors();
   }
   protected function loadActors() {
-    return Actor::all();
+    $query = Actor::all();
+
+
+
+    if ($this->property('sortOrder') == 'name asc' ) {
+      $query = $query->sortBy('name');
+    } else {
+      $query = $query->sortByDesc('name');
+    }
+
+
+    if ($this->property('results') > 0) {
+      $query = Actor::all()->take($this->property('results'));
+    }
+
+    return $query;
   }
 
 
